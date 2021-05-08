a9ae91ba9f6df2a8e39436be02978847e1e39a20
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-05-08 22:14:07 +0700

    get soil data to dropdown list

--- app/src/main/java/com/desi/expertplantapp/ui/check/CheckFormFragment.kt
+++ app/src/main/java/com/desi/expertplantapp/ui/check/CheckFormFragment.kt
@@ -1,14 +1,13 @@
 package com.desi.expertplantapp.ui.check
 
+import android.R
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import com.desi.expertplantapp.data.Soil
 import com.desi.expertplantapp.databinding.FragmentCheckFormBinding
 import com.google.firebase.database.*
@@ -17,8 +16,8 @@ class CheckFormFragment : Fragment() {
 
     private lateinit var fragmentCheckFormBinding: FragmentCheckFormBinding
     private lateinit var database: DatabaseReference
-    private var arrayAdapter: ArrayAdapter<String> ? = null
-    private var listSoils = arrayOf("tanah liat", "tanah gambut", "tanah aluvial")
+    private lateinit var mapSoils: LinkedHashMap<String, String>
+    private var arrayAdapter: ArrayAdapter<String>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentCheckFormBinding = FragmentCheckFormBinding.inflate(layoutInflater, container, false)
@@ -28,25 +27,32 @@ class CheckFormFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arrayAdapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_dropdown_item, listSoils) }
-        fragmentCheckFormBinding.autoCompleteTextView.setAdapter(arrayAdapter)
-
+        mapSoils = LinkedHashMap()
         getListSoils()
+
     }
 
     private fun getListSoils() {
+        val listSoils: MutableList<String> = ArrayList()
         database = FirebaseDatabase.getInstance().getReference("soils")
         database.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
                     for (soilSnapshot in snapshot.children) {
-
+                        val soil = soilSnapshot.getValue(Soil::class.java)
+                        mapSoils.put(soilSnapshot.key!!, soil?.name!!)
+                        listSoils.add(soil.name)
                     }
+                    Log.d("firebase", "map soils = $mapSoils")
                 }
-            }
+                arrayAdapter = context?.let { ArrayAdapter(it, R.layout.simple_spinner_dropdown_item, listSoils) }
+                fragmentCheckFormBinding.autoCompleteTextView.setAdapter(arrayAdapter)
+                //hashMapAdapter = LinkedHashMapAdapter<String, String>(this, R.layout.simple_spinner_dropdown_item, mapSoils)
+                //fragmentCheckFormBinding.autoCompleteTextView.setAdapter(hashMapAdapter)
+                }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.d("firebase", "gagal")
+                Log.d("firebase", "cancelled")
             }
         })
     }
